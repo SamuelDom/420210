789ee73516a6058db83914bd56dcb3b2ef613119
Samuel Richard Domesca <2390145@collegemv.qc.ca>
2025-01-30 11:52:14 -0500

    Ajout fichier java commit

---
+++ nouveau 1.java
@@ -0,0 +1,10 @@
+public class Exo_17{
+	public static void main{
+	
+	Char [] Voyelles = {'a','e','i','o','u','y'};
+	Char [] caracteres = {'a','b','e','f','i','o','u','p'};
+	
+		
+		System.out.println("Il y a " + "voyelles dans le tableau");
+	}
+}
